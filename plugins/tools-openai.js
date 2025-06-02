9af34433636dd025369ffb4f719265d67736de4e
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥𝐙𝐤𝐬 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-06-02 07:23:25 -0300

    Actualización de la Inteligencia Artificial [ Senna - Ai ]

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -1,19 +1,18 @@
-import fetch from 'node-fetch'
+import tools from '@takanashi-soft/tools'
+
 let handler = async (m, { conn, text }) => {
 if (!text) throw `✳️ ${mssg.notext}`
 m.react('💬')
-
-    let syst = `Eres Senna Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
-     try {
-       let gpt = await fetch(`https://skynex.boxmine.xyz/docs/ai/myprompt?text=${text}&prompt=${syst}&apikey=Dylux`)
-        let res = await gpt.json()
-        await m.reply(res.answer, null, fwc)
-	} catch {
-        m.reply(`❎ Error: intenta más tarde`)
-     }
+let syst = `Eres Senna Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
+try {
+let res = await tools.ai.mylogic(text, syst);
+await m.reply(res.answer, null, fwc)
+} catch {
+m.reply(`❎ Error: intenta más tarde`)
+ }
 }
 handler.help = ['ai <text>']
 handler.tags = ['tools']
 handler.command = ['ia', 'ai', 'chatgpt', 'openai', 'gpt']
 
-export default handler
+export default handler;
